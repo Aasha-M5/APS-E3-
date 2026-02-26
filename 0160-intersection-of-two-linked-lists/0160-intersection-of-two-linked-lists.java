52bc350ef50625175b41c150d944f2413cc5d676
Aasha-M5 <mamilla.aashanss5@gmail.com>
2026-02-27 00:15:58 +0530

    Time: 2 ms (52.26%), Space: 53 MB (42.43%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode pA = headA;
+        ListNode pB = headB;
+
+        // When either pointer reaches the end, redirect it to the other head
+        while (pA != pB) {
+            pA = (pA == null) ? headB : pA.next;
+            pB = (pB == null) ? headA : pB.next;
+        }
+
+        // Either intersection node or null
+        return pA;
+    }
+}
